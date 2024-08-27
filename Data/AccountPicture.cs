17e843d23d9b2b6d3cd1160a1bc7484589d85803
Travis Campbell <traviscampbell7777@gmail.com>
2024-08-26 23:58:15 -0700

    edit profile button and small await fixes

---
+++ Data/AccountPicture.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AlbumDatabaseServer.Data
+{
+	public class AccountPicture
+	{
+	}
+}
